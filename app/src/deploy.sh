0c62340c8d5105c825140f7a1ac1aa3a089a4ffd
Konrad Fedorczyk <contact@realhe.ro>
2021-12-22 10:28:44 +0100

    🔨 Add composer to deploy script

--- app/src/deploy.sh
+++ app/src/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 mydir="${0%/*}"
+echo "Running composer"
+composer install -d "$mydir"
+
 echo "Running deployment of additional files"
 cp "$mydir"/deploy/wp-config.php "$mydir"/wordpress/
 rm -r "$mydir"/wordpress/wp-content
