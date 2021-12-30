b73efac3050b34d0350f4eed12b71158c7b636a8
Konrad Fedorczyk <contact@realhe.ro>
2021-12-30 16:12:22 +0100

    Update git config

--- bin/__git.sh
+++ bin/__git.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo -e "${green}🍿 Running git to fetch updates${clear}"
 git fetch
+if [ $GIT_FORCE = "true" ]; then
+  git reset --hard origin/"${BRANCH}"
+fi
 git pull
 git submodule update --init --remote
