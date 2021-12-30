ea4c4fb2ce0eec84a739456b254d630d7d6b2298
Konrad Fedorczyk <contact@realhe.ro>
2021-12-30 13:42:50 +0100

    Update deploy scripts

--- initial-deploy-docker.sh
+++ initial-deploy-docker.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-mydir="${0%/*}"
+mydir="${0%/*}/app"
 echo "Running composer"
 composer install -d "$mydir"/dist
+
+if [ -f "$mydir"/dist/wp-content/plugins/headless-cms-plugin/headless-cms-plugin.php ]; then
+  composer install -d "$mydir"/dist/wp-content/plugins/headless-cms-plugin
+else
+  echo "Error: please clone this repository with submodules!"
+fi
 
 echo "Running deployment of additional files"
 cp "$mydir"/deploy/wp-config.php "$mydir"/dist/wordpress/
---
+++ initial-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+mydir="${0%/*}/app"
+echo "Running composer"
+composer install -d "$mydir"/dist
+
+if [ -f "$mydir"/dist/wp-content/plugins/headless-cms-plugin/headless-cms-plugin.php ]; then
+  composer install -d "$mydir"/dist/wp-content/plugins/headless-cms-plugin
+else
+  echo "Error: please clone this repository with submodules!"
+fi
+
+echo "Running deployment of additional files"
+cp "$mydir"/deploy/wp-config.php "$mydir"/dist/wordpress/
+rm -r "$mydir"/dist/wordpress/wp-content
+
+echo "Remember to create .env file in the dist directory!"
