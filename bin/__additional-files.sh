0c0e8cdf327982e59429cfc4465af0a7c5db20af
Konrad Fedorczyk <contact@realhe.ro>
2022-01-07 15:32:04 +0100

    Update scripts

--- bin/__additional-files.sh
+++ bin/__additional-files.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo -e "${magenta}🔥 Running deployment of additional files${clear}"
 cp "$rootdir"/app/deploy/wp-config.php "$rootdir"/app/dist/wordpress/
-rm -r "$rootdir"/app/dist/wordpress/wp-content
+
+if [ -d "$rootdir"/app/dist/wordpress/wp-content ]; then
+  rm -r "$rootdir"/app/dist/wordpress/wp-content
+fi
